ca456d9304756e7dce6e060bd508353a7bbaf7f9
Neil Macneale V <neil.macneale.v@gmail.com>
2018-12-24 17:17:12 -0800

    Added GPS support to location_service. See README for pins

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ Location service and motor service are two projects, for the two arduinos onboar
 The location service will constantly send information to the jetson, so it can just pull from that serial port whenever it needs GPS or compass data.
 
 The motor service will never send any information to the jetson, but will just take commands from the jetson or RC receiver, and control the motors.
+
+Pins:
+  Location service arduino:
+    GPS connects TX  to pin 3.
+    CP  connects SDA to pin A4.
+    CP  connects SDL to pin A5.
+  Motor service arduino:
+    RM ESC to pin 2.
+    LM ESC to pin 3.
--- location_service/src/main.cpp
+++ location_service/src/main.cpp
@@ -1,8 +1,19 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <LSM303.h>
+#include <SoftwareSerial.h>
+#include <Adafruit_GPS.h>
+
+#define GPSECHO  true
 
 LSM303 compass;
+
+SoftwareSerial mySerial(3, 2);
+Adafruit_GPS GPS(&mySerial);
+boolean usingInterrupt = false;
+uint32_t timer = millis();
+
+void useInterrupt(boolean);
 
 void setup() {
   Serial.begin(115200);
@@ -11,30 +22,88 @@ void setup() {
   compass.enableDefault();
   compass.m_min = LSM303::vector<int16_t> {-280, -520, -780};
   compass.m_max = LSM303::vector<int16_t> {200, 0, -660};
+  // setup GPS
+  GPS.begin(9600);
+
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+  GPS.sendCommand(PGCMD_ANTENNA);
+
+  useInterrupt(true);
+
+  delay(1000);
+  mySerial.println(PMTK_Q_RELEASE);
 }
 
-int getCompass() {
+SIGNAL(TIMER0_COMPA_vect) {
+  char c = GPS.read();
+  #ifdef UDR0
+    if (GPSECHO) {
+      if (c) {
+        UDR0 = c;
+      }
+    }
+  #endif
+}
+
+void useInterrupt(boolean v) {
+  if (v) {
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+    usingInterrupt = true;
+  } else {
+    TIMSK0 &= ~_BV(OCIE0A);
+    usingInterrupt = false;
+  }
+}
+
+float getCompass() {
   compass.read();
   return compass.heading();
 }
 
-int getGPSLat() {
-  return 0;
+double getGPSLat() {
+  return GPS.latitudeDegrees;
 }
 
-int getGPSLon() {
-  return 0;
+double getGPSLon() {
+  return GPS.longitudeDegrees;
 }
 
 void loop() {
-  Serial.print("[CP");
-  Serial.print(getCompass());
-  Serial.print("]");
+  if (! usingInterrupt) {
+    char c = GPS.read();
+    if (GPSECHO) {
+      if (c) {
+        // Serial.print(c);
+      }
+    }
+  }
 
-  Serial.print("[GP");
-  Serial.print(getGPSLat());
-  Serial.print(",");
-  Serial.print(getGPSLon());
-  Serial.println("]");
-  delay(100);
+  if (GPS.newNMEAreceived()) {
+
+    if (!GPS.parse(GPS.lastNMEA()))
+    return;
+  }
+
+  if (timer > millis()) {
+    timer = millis();
+  }
+
+  if (millis() - timer > 2000) {
+    timer = millis();
+    if (GPS.fix) {
+      Serial.print("[CP");
+      Serial.print(getCompass());
+      Serial.print("]");
+
+      Serial.print("[GP");
+      Serial.print(getGPSLat(), 8);
+      Serial.print(",");
+      Serial.print(getGPSLon(), 8);
+      Serial.println("]");
+      delay(100);
+    }
+  }
+
 }
